41f7386c25562aa3370a6224617982b5b560bd7e
366-konstantinos <konstantinos.s@gtgroup.io>
2024-06-15 11:17:34 +0300

    366

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,43 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1001,
+    config = function()
+      vim.cmd.colorscheme 'kanagawa'
+    end,
+    init = function()
+      vim.cmd.colorscheme 'kanagawa'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
+  {
+    'adalessa/laravel.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'tpope/vim-dotenv',
+      'MunifTanjim/nui.nvim',
+      'nvimtools/none-ls.nvim',
+    },
+    cmd = { 'Sail', 'Artisan', 'Composer', 'Npm', 'Yarn', 'Laravel' },
+    keys = {
+      { '<leader>la', ':Laravel artisan<cr>' },
+      { '<leader>lr', ':Laravel routes<cr>' },
+      { '<leader>lm', ':Laravel related<cr>' },
+    },
+    event = { 'VeryLazy' },
+    config = true,
+  },
+  {
+    'stevearc/aerial.nvim',
+    config = function()
+      require('aerial').setup()
+    end,
+  },
+}
